3cee620fc16e4e4755502cb5bb6f4109ba06ab69
chaester <yubs1004@gmail.com>
2017-05-12 15:29:08 +0900

    상품 도메인 추가

---
+++ src/main/java/ctx/BatchJob.java
@@ -0,0 +1,7 @@
+package ctx;
+
+/**
+ * Created by chaester on 2017-05-10.
+ */
+public class BatchJob {
+}
---
+++ src/main/java/ctx/domain/Item.java
@@ -0,0 +1,26 @@
+package ctx.domain;
+
+/**
+ * Created by chaester on 2017-05-10.
+ */
+public class Item {
+    private String id;
+    private String pname;
+    private Integer price_pc;
+    private Integer price_mobile;
+    private Integer normal_price;
+    private String  link;
+    private String  mobile_link;
+    private String  image_link;
+    private String  add_image_link;
+    private String  category_name1;   // 대분류 (제휴사 카테고리명)
+    private String  category_name2;   // 중분류 (제휴사 카테고리명)
+    private String  category_name3;   // 소분류 (제휴사 카테고리명)
+    private String  category_name4;   // 세분류 (제휴사 카테고리명)
+    private String  naver_category;   // 네이버카테고리
+    private String  naver_product_id; // 가격비고 페이지 ID
+    private String  condition;        // 상품상태
+    private String  import_flag;      // 해외구매대행 여부
+
+
+}
